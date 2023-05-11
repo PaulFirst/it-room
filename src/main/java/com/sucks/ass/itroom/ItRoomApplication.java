9bf4377092431752fa47e19c6dcf6fcc052f1e1b
Stanislav <Natalin>
2023-05-11 18:30:50 +0300

    init commit

---
+++ src/main/java/com/sucks/ass/itroom/ItRoomApplication.java
@@ -0,0 +1,13 @@
+package com.sucks.ass.itroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sucks/ass/itroom/ItRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sucks.ass.itroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
