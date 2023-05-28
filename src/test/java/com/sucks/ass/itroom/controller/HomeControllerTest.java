c0424eb03d9199db779606f019d7b256fe043561
Stanislav <Natalin>
2023-05-28 22:48:08 +0300

    IRC-2 Prepare primitive unit test Add HomeControllerTest

---
+++ src/test/java/com/sucks/ass/itroom/controller/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package com.sucks.ass.itroom.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("message"))
+                .andExpect(MockMvcResultMatchers.model().attribute("message", "Приветствуем Вас на платформе IT-room!"));
+    }
+}
